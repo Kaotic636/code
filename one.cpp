5b5fd36ad5cabc1ffe783f3832dfcf9b65ede991
Kaotic636 <Djed-636@hotmail.com>
2020-07-05 20:24:55 -0500

    Create one.cpp

---
+++ one.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int number1;
+    int number2;
+    int result;
+
+    cin >> number1;
+    cin >> number2;
+    
+    result = number1 + number2;
+    
+    cout << result << endl;
+
+    return 0;
+}
